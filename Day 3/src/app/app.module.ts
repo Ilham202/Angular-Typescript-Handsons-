c38f07178558d15b687da3ef4f9238e6bfb961f0
Ilham202 <42730789+Ilham202@users.noreply.github.com>
2021-07-11 13:33:39 +0530

    Update app.module.ts

--- Day 3/src/app/app.module.ts
+++ Day 3/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
 import { EditEmpComponent } from './edit-emp/edit-emp.component';
 import { EditEmpTemplateDrivenComponent } from './edit-emp-template-driven/edit-emp-template-driven.component';
+import { EditEmpReactiveComponent } from './edit-emp-reactive/edit-emp-reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewEmpComponent,
     EditEmpComponent,
-    EditEmpTemplateDrivenComponent
+    EditEmpTemplateDrivenComponent,
+    EditEmpReactiveComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
