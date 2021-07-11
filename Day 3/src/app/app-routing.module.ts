f4f504754e51e5ebe34a515aba08300bd01df82f
Ilham202 <42730789+Ilham202@users.noreply.github.com>
2021-07-11 13:35:44 +0530

    Update app-routing.module.ts

--- Day 3/src/app/app-routing.module.ts
+++ Day 3/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditEmpComponent } from './edit-emp/edit-emp.component';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
 import { EditEmpTemplateDrivenComponent } from './edit-emp-template-driven/edit-emp-template-driven.component';
+import {EditEmpReactiveComponent} from './edit-emp-reactive/edit-emp-reactive.component';
 
 const routes: Routes = [{path:'view-emp',component:ViewEmpComponent},
                         {path:'edit-emp',component:EditEmpComponent},
-                        {path:'edit-emp-template-driven',component:EditEmpTemplateDrivenComponent}
+                        {path:'edit-emp-template-driven',component:EditEmpTemplateDrivenComponent},
+                        {path:'edit-emp-reactive',component:EditEmpReactiveComponent}
                       ];
 
 @NgModule({
